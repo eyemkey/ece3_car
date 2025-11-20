7fc5f9f1932d6c0feb4064b3d34f1cf5ecd9d501
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2025-11-20 13:40:37 -0800

    Removed /header /src

--- ece3_car/header/error_calc.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-float calculateError(  uint16_t *sensorValues,
-                        uint16_t *minValues,
-                        uint16_t *ofMaxValues,
-                        const int8_t *weights,
-                        const uint8_t NUM_SENSORS);
